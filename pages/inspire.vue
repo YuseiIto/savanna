a9217ec65567550b67bbf8c0d2784beaf98ab66d
Yusei Ito <yuseikun@Outlook.jp>
2020-05-07 21:45:24 +0900

    OGP setting

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -13,3 +13,39 @@
     </v-flex>
   </v-layout>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      meta: {
+        title: 'Test-Page!',
+        description: 'ページ個別のディスクリプション',
+        type: 'article',
+        url: 'https://example.com/test',
+        image: 'https://example.com/img/ogp/test.jpg'
+      }
+    }
+  },
+  head() {
+    return {
+      title: this.meta.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.meta.description
+        },
+        { hid: 'og:type', property: 'og:type', content: this.meta.type },
+        { hid: 'og:title', property: 'og:title', content: this.meta.title },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.meta.description
+        },
+        { hid: 'og:url', property: 'og:url', content: this.meta.url },
+        { hid: 'og:image', property: 'og:image', content: this.meta.image }
+      ]
+    }
+  }
+}
+</script>
