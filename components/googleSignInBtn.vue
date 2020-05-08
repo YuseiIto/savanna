c3f5680d4c31b83eb2f87805594471672b5df4a8
Yusei Ito <yuseikun@Outlook.jp>
2020-05-08 14:16:28 +0900

    Feature: google sign in button

---
+++ components/googleSignInBtn.vue
@@ -0,0 +1,12 @@
+<template>
+  <img
+    src="/googleSignIn.png"
+    class="google-signin-button"
+    @click="$emit('click')"
+  />
+</template>
+<style>
+.google-signin-button {
+  width: 250px;
+}
+</style>
