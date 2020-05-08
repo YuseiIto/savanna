4b2ea5fe1e24ba47d959935365182718a5ba933a
Yusei Ito <yuseikun@Outlook.jp>
2020-05-08 19:22:52 +0900

    Not logged In page

---
+++ components/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <logo />
+    <h3 class="title is-5">Sign in to start NOW!</h3>
+    <google-sign-in-btn @click="signIn" />
+  </div>
+</template>
+<script>
+import logo from "./logo";
+import googleSignInBtn from "./googleSignInBtn";
+import firebase from "../plugins/firebase";
+
+export default {
+  components: { logo, googleSignInBtn },
+  methods: {
+    signIn() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
+    }
+  }
+};
+</script>
