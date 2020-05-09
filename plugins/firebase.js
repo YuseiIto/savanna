501ec450666b93089a5fbcd2ea633cf181aca922
Yusei Ito <yuseikun@Outlook.jp>
2020-05-09 17:08:56 +0900

    Install firestore

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,6 +1,7 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCctelbPQNM__kbx6qNCLnBKtp7wlYWGKk",
