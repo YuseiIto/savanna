089d42ce6fbc2787d9b4247c72a46022ffbe68e9
Yusei Ito <yuseikun@Outlook.jp>
2020-05-09 12:12:16 +0900

    Username validation

--- components/register.vue
+++ components/register.vue
@@ -4,7 +4,7 @@
     <h6 class="title is-5">New to Savannah?</h6>
     <h3 class="title is-3">Register username</h3>
     <input type="text" placeholde="Put your username" v-model="username" />
-    <p v-if="exists" style="color:#E51616">This username is already taken.</p>
+    <p v-if="!isValidUsername" style="color:#E51616">{{errorMessage}}</p>
     <input type="button" value="Register" @click="register" />
   </div>
 </template>
@@ -12,7 +12,6 @@
 <script>
 import firebase from "~/plugins/firebase";
 import logo from "./logo";
-import isUserExists from "~/plugins/isUserExists";
 
 export default {
   components: {
@@ -22,43 +21,71 @@ export default {
     return {
       db: null,
       username: "",
-      exists: false
+      errorMessage: "",
+      isValidUsername: false
     };
   },
   mounted() {
     this.db = firebase.firestore();
   },
-  methods: {
-    register() {
-      this.exists = false;
-      isUserExists(this.username).then(res => {
-        this.exists = res;
+  watch: {
+    username: function(val) {
+      const pattern = /^[-0-9a-zA-Z_.]+$/g;
+      const result = val.match(pattern) == val;
 
-        if (!this.exists) {
-          //When the username available
-          const that = this;
-
+      if (val === "") {
+        this.errorMessage = "Username is required";
+        return false;
+      } else {
+        if (result) {
           this.db
             .collection("users")
-            .add({
-              username: this.username,
-              uid: firebase.auth().currentUser.uid,
-              lowerUsername: this.username.toLowerCase(),
-              bio: "",
-              followings: [],
-              projects: [],
-              photoURL: firebase.auth().currentUser.photoURL,
-              name: firebase.auth().currentUser.displayName
-            })
-            .then(docRef => {
-              that.$store.dispatch("setDbUid", docRef.id);
-              that.$emit("complete");
-            })
-            .catch(function(error) {
-              console.error("Error adding document: ", error);
+            .where("username", "==", val)
+            .get()
+            .then(snapshot => {
+              if (snapshot.empty) {
+                this.errorMessage = "";
+                this.isValidUsername = true;
+              } else {
+                this.errorMessage = "This username is already taken";
+                this.isValidUsername = false;
+              }
             });
+        } else {
+          this.errorMessage = "The username contains invalid character(s).";
+          this.isValidUsername = false;
         }
-      });
+      }
+    }
+  },
+  methods: {
+    register() {
+      console.log("Called");
+      if (this.isValidUsername) {
+        //When the username available
+        console.log("Called");
+        const that = this;
+
+        this.db
+          .collection("users")
+          .add({
+            username: this.username,
+            uid: firebase.auth().currentUser.uid,
+            lowerUsername: this.username.toLowerCase(),
+            bio: "",
+            followings: [],
+            projects: [],
+            photoURL: firebase.auth().currentUser.photoURL,
+            name: firebase.auth().currentUser.displayName
+          })
+          .then(docRef => {
+            that.$store.dispatch("setDbUid", docRef.id);
+            that.$emit("complete");
+          })
+          .catch(function(error) {
+            console.error("Error adding document: ", error);
+          });
+      }
     }
   }
 };
