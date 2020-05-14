2ae42c345225365c8bb78fae233ff92a80313e63
Yusei Ito <yuseikun@Outlook.jp>
2020-05-14 16:28:51 +0900

    FIx key

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -4,14 +4,15 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCctelbPQNM__kbx6qNCLnBKtp7wlYWGKk",
+    apiKey: "AIzaSyBLf14cq8s2ba8pvRGfjH5ZL_2-aJiQ400",
     authDomain: "savannah-app.firebaseapp.com",
     databaseURL: "https://savannah-app.firebaseio.com",
     projectId: "savannah-app",
     storageBucket: "savannah-app.appspot.com",
     messagingSenderId: "1034099100594",
     appId: "1:1034099100594:web:2eacf7b892cfd39d31b2ab",
-    measurementId: "G-D5VKV3N6VB"
+    measurementId: "G-EFKKW8P2JR"
+
 };
 
 if (!firebase.apps.length) {
