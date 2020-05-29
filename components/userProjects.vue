cfe142058adf90d38b8ca412c8483052546b0f7d
Yusei Ito <yuseikun@Outlook.jp>
2020-05-29 21:23:10 +0900

    Modify: userpage

---
+++ components/userProjects.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="width:500px;">
+    <ul>
+      <li v-for="project in projects" :key="project.name" class="project-container">
+        <div style="text-align:left;">
+          <nuxt-link :to="`/${$route.params.username}/${project.name}`">
+            <h3
+              class="title is-6"
+              style="margin-bottom:10px;border-bottom:1px solid #333333; padding-bottom:5px;"
+            >{{project.name}}</h3>
+          </nuxt-link>
+          <p>{{project.description}}</p>
+        </div>
+
+        <savannah :projectData="project" />
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import savannah from "./savannah";
+export default {
+  props: ["projects"],
+  components: {
+    savannah
+  },
+  mounted() {
+    console.log(this.projects);
+  }
+};
+</script>
+<style>
+.project-container {
+  margin-top: 30px;
+}
+</style>
--- pages/_username/index.vue
+++ pages/_username/index.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="container">
     <userHeader v-if="!notFound" :dbUser="dbUserData" />
+    <userProjects :projects="userProjects" />
   </div>
 </template>
 <script>
 import firebase from "../../plugins/firebase";
 import userHeader from "../../components/userHeader";
+import userProjects from "../../components/userProjects";
+
 export default {
   data() {
-    return { dbUserData: null, notFound: true };
+    return { dbUserData: null, notFound: true, userProjects: [] };
   },
   components: {
-    userHeader
+    userHeader,
+    userProjects
   },
   mounted() {
     const that = this;
@@ -28,6 +32,19 @@ export default {
           snapshot.forEach(doc => {
             that.dbUserData = doc.data();
           });
+
+          // Get projetcs
+          const projects = [];
+          firebase
+            .firestore()
+            .collection("projects")
+            .where("author", "==", that.dbUserData.uid)
+            .get()
+            .then(snapshot => {
+              snapshot.forEach(e => {
+                that.userProjects.push(e.data());
+              });
+            });
         }
       });
   }
@@ -37,5 +54,6 @@ export default {
 .container {
   display: flex;
   flex-direction: column;
+  align-items: center;
 }
 </style>
